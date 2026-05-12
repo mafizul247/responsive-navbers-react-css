6a810c6e9424a90a45e81c6722d0e12369656b54
mafizul247 <mafizul247@gmail.com>
2026-05-12 16:23:01 +0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import Navber3 from './components/Navber3/Navber3';
 // import Navber1 from "./components/Navber1/Navber1";
 // import Navber2 from "./components/Navber2/Navber2";
-import Navber4 from "./components/Navber4/Navber4";
 
 const App = () => {
   return (
